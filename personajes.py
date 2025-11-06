46f054b8738b4db0a40edf136a831650700dee7c
xMarcosZz <marcos10para@gmail.com>
2025-11-06 21:43:57 +0100

    Actualización

--- personajes.py
+++ personajes.py
@@ -1,3 +1,16 @@
+import pyxel
+
 class Personaje:
-    def __init__(self, nombre):
+    def __init__(self, nombre, x, y, u, v):
         self.nombre = nombre
+        self.x = x
+        self.y = y
+        self.u = u
+        self.v = v
+
+    def dibujar(self):
+        pyxel.blt(self.x, self.y, 0, self.u, self.v, 16, 16)
+
+
+mario = Personaje("Mario", 120, 100, 0, 0)
+luigi = Personaje("Luigi", 120, 100, 16, 0)
